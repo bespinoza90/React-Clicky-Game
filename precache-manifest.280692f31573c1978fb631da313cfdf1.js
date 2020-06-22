a7bc0d7998d67253286bbe7a893c315ecfb8877d
Bobby Espinoza <bobbyespinoza@gmail.com>
2020-06-22 12:17:38 -0700

    Updates

---
+++ precache-manifest.280692f31573c1978fb631da313cfdf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b59e2a7e5367130d0378720b53540eaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df27855918bf6a57f288",
+    "url": "/static/css/main.0ee60457.chunk.css"
+  },
+  {
+    "revision": "82a97b2775889fdb9253",
+    "url": "/static/js/2.311c529f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.311c529f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df27855918bf6a57f288",
+    "url": "/static/js/main.9ddaa6ec.chunk.js"
+  },
+  {
+    "revision": "8253bdd9bb53c64a6305",
+    "url": "/static/js/runtime-main.055e8b8b.js"
+  }
+]);
